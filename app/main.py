5f402475d2b40620947756db83eb3692c9af2de8
nwakalordivine <divinenwakalor31@gmail.com>
2025-09-13 17:49:30 +0100

    third  commit

--- app/main.py
+++ app/main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db import init_db
 from app.routers import pest, disease, climate
 
+
 app = FastAPI(title="Agri AI Services", version="1.0")
+
+# Enable CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 def on_startup():
     init_db()
 
+
 app.include_router(pest.router)
 app.include_router(disease.router)
 app.include_router(climate.router)
+
 
 @app.get("/")
 def root():
